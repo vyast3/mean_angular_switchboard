d17324fbe2fbc9154a1e55accf207cd026bedfd1
tanvi <tanvivyas1987@gmail.com>
2017-10-22 20:38:08 -0700

    switchboard

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  switches = [true, true, true, true, true, true, true, true, true, true];
+  flipSwitch(idx) {
+    this.switches[idx] = !this.switches[idx];
+  }
+  
+}
+
+
+
